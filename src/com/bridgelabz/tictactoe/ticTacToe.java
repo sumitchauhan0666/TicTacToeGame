1414862afb68dca3a55918d82e5a451eb3c39a14
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-18 19:56:38 +0530

    uc1 Creating_Tic_Tac_Toe_Board

---
+++ src/com/bridgelabz/tictactoe/ticTacToe.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.tictactoe;
+
+public class ticTacToe {
+ public static char[] gameArray = new char[10];
+
+ public static void ticTacToe() {
+  for (int i = 0; i < gameArray.length; i++) {
+   gameArray[i] = ' ';
+  }
+ }
+}
