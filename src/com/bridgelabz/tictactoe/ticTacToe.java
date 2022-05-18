48c32631c1fc54a808abd4a9f9754bc481a88de5
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-18 20:00:18 +0530

    Allow_players_to_choose_letter_X or O

--- src/com/bridgelabz/tictactoe/ticTacToe.java
+++ src/com/bridgelabz/tictactoe/ticTacToe.java
@@ -1,12 +1,25 @@
 package com.bridgelabz.tictactoe;
+import java.util.Scanner;
 
 public class ticTacToe {
-
  public static char[] gameArray = new char[10];
+ private static char userTurn;
+ private static char computerTurn;
+ public static Scanner scanner = new Scanner(System.in);
 
  public static void ticTacToe() {
   for (int i = 0; i < gameArray.length; i++) {
    gameArray[i] = ' ';
   }
+ }
+ public static char user(Scanner scanner) {
+  System.out.println("Enter your choice  X or O only");
+  return scanner.next().toUpperCase().charAt(0);
+ }
+
+ public static void main(String[] args) {
+  ticTacToe();
+  userTurn = user(scanner);
+  computerTurn = (userTurn == 'X') ? 'O' : 'X';
  }
 }
